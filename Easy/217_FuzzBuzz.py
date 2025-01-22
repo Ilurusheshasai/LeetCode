817cc30a83ebddd751ca11e2a9a43c6f4d8edfc1
Sai Iluru <ilurusheshasai@gmail.com>
2025-01-22 15:30:21 -0500

    Improved with string comprehension, may be confusing but consise

--- Easy/217_FuzzBuzz.py
+++ Easy/217_FuzzBuzz.py
@@ -1,14 +1,3 @@
-## Solution using numpy
-
-import numpy as np
-
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        numbers = np.arange(1, n + 1)
-        result = np.where((numbers % 3 == 0) & (numbers % 5 == 0), 'FizzBuzz',
-                 np.where(numbers % 3 == 0, 'Fizz',
-                 np.where(numbers % 5 == 0, 'Buzz', numbers.astype(str))))
-        return result.tolist()
-
-# When to use it
-#  --> When we have larger n value due to vectorized operations
+        return ['Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0) or str(i) for i in range(1, n + 1)]
