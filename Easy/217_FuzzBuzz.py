b7aa7e0257dc76ab8e2056073edf8422ce7c899f
Sai Iluru <ilurusheshasai@gmail.com>
2025-01-22 15:29:03 -0500

    Improved with Numpy

--- Easy/217_FuzzBuzz.py
+++ Easy/217_FuzzBuzz.py
@@ -1,13 +1,14 @@
+## Solution using numpy
+
+import numpy as np
+
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        l = []
-        for i in range(1, n+1):
-            if i % 3 == 0 and (i) % 5 == 0:
-                l.append('FizzBuzz')
-            elif i % 3 == 0:
-                l.append('Fizz')
-            elif i % 5 == 0:
-                l.append('Buzz')
-            else:
-                l.append(str(i))
-        return l
+        numbers = np.arange(1, n + 1)
+        result = np.where((numbers % 3 == 0) & (numbers % 5 == 0), 'FizzBuzz',
+                 np.where(numbers % 3 == 0, 'Fizz',
+                 np.where(numbers % 5 == 0, 'Buzz', numbers.astype(str))))
+        return result.tolist()
+
+# When to use it
+#  --> When we have larger n value due to vectorized operations
