2745179c6ce2fc1f4746abc8fde6c09cda7c2e9e
Sai Iluru <ilurusheshasai@gmail.com>
2025-02-15 13:06:12 -0500

    191 number of one bits and time complexity

--- Easy/misc/hamming_code.py
+++ Easy/misc/hamming_code.py
@@ -1,11 +1,10 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
-        while n > 0:
-            if n % 2 == 1:
-                count += 1
-            n = n//2
-
+        while n:
+            n &= n - 1  # Clears the least-significant 1-bit
+            count += 1
         return count
+
     
-# time complexity - O(logn)
+# time complexity - O(k) where k is the number of 1-bits in the binary representation of the input number n.
